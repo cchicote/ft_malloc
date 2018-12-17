501382036540d7ba95baa6c1d0b777e0eeb6c400
Côme Chicoteau <cchicoteau@MacBook-Pro-de-Come.local>
2018-12-17 13:12:42 +0100

    Tests added

---
+++ tests/run_tests.sh
@@ -0,0 +1,32 @@
+echo "Test 0"
+/usr/bin/time -l tests/run.sh tests/test0
+
+echo "Test 1"
+/usr/bin/time -l tests/run.sh tests/test1
+
+echo "Test 2"
+/usr/bin/time -l tests/run.sh tests/test2
+
+echo "Test 3"
+/usr/bin/time -l tests/run.sh tests/test3
+
+echo "Test 4"
+/usr/bin/time -l tests/run.sh tests/test4
+
+echo "Test 5"
+/usr/bin/time -l tests/run.sh tests/test5
+
+echo "Test 6"
+/usr/bin/time -l tests/run.sh tests/test6
+
+echo "Test 7"
+/usr/bin/time -l tests/run.sh tests/test7
+
+echo "Test 8"
+/usr/bin/time -l tests/run.sh tests/test8
+
+echo "Test 9"
+/usr/bin/time -l tests/run.sh tests/test9
+
+echo "Test 10"
+/usr/bin/time -l tests/run.sh tests/test10
